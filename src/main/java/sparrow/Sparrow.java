b36142f88bc74e22458cd1b28df4cb640d155594
sean-vanluvender <6jV8rs$QGuUn9fJ>
2022-03-10 11:39:54 -0500

    New unknown operating system prompt

--- src/main/java/sparrow/Sparrow.java
+++ src/main/java/sparrow/Sparrow.java
@@ -11,11 +11,18 @@ import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Device;
 import org.eclipse.swt.layout.*;
 import org.eclipse.swt.*;
+import org.eclipse.swt.widgets.Text;
+
 import sparrow.http.SparrowClient;
 
 public class Sparrow {
 
     public static void main(String[] args) {
+        String os = System.getProperty("os.name");
+        System.out.println(os);
+        if (os.equals("Windows 10")) {
+            unknownos(os);
+        }
         SparrowClient sparrowClient = new SparrowClient("http://127.0.0.1:8000/api/v1/alert",
                 "CREATE_TOKEN_AND_PUT_IT_HERE");
         if (sparrowClient.raiseAlert()) {
@@ -47,6 +54,36 @@ public class Sparrow {
                 display.sleep();
         }
         display.dispose(); //end program, save resources
+    }
+    public static void unknownos(String os) {
+        Display display = new Display();
+        Shell shell = new Shell();
+        Device device = Display.getCurrent();
+
+        String user = System.getProperty("user.name");
+        Image test = new Image(display, Sparrow.class.getResourceAsStream("/unknownos.png"));
+
+        shell.setBackgroundImage(test);
+
+        Text text = new Text(shell, SWT.NO_BACKGROUND);
+        text.setBounds(10, 10, 500, 50);
+        text.append("Your Operating System, " + os + ", is not supported on Sparrow.");
+        text.setEditable(false);
+
+        shell.setText("Unknown Operating System // Sparrow"); //Set text at top
+        shell.setMaximumSize(960,680);
+        shell.setMinimumSize(960,680);
+
+        shell.open(); //Start window
+
+        display = Display.getCurrent();
+
+        while(!shell.isDisposed()) {
+            if(!display.readAndDispatch())
+                display.sleep();
+        }
+        display.dispose(); //end program, save resources
+        System.exit(0);
     }
     public static void error() {
         Display display = new Display();
