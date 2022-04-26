9f0f87ff6c6b8bad5de848faf7ff1a161d66892a
seanv <vanluvenderseanw@scps.net>
2022-04-26 08:11:34 -0400

    Configuration reader set to the documents' folder. Added diagnostics capabilities that will be expanded in future versions. May not be suitable for linux. (yet)

--- src/main/java/sparrow/http/SparrowClient.java
+++ src/main/java/sparrow/http/SparrowClient.java
@@ -1,6 +1,9 @@
 package sparrow.http;
 
 import java.io.IOException;
+import java.io.File;
+import java.io.FileWriter;
+import java.time.ZoneId;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -12,6 +15,7 @@ public class SparrowClient {
 
     private String endpoint;
     private String token;
+    private String exception;
     private HttpClient client;
 
     public SparrowClient(String endpoint, String token) {
@@ -21,8 +25,19 @@ public class SparrowClient {
                 .version(HttpClient.Version.HTTP_1_1)
                 .build();
     }
+    public static void createFile(int i) throws IOException {
+        String date = java.time.LocalDate.now().toString();
+        String time = java.time.LocalTime.now(ZoneId.of("America/New_York")).toString();
+        String timereplaced = time.replaceAll(":","-");
+        File error = new File("C:\\Users\\seanv\\Documents\\Sparrow\\diagnostics\\" + date + "+" + timereplaced + ".txt");
+        error.createNewFile();
 
-    public boolean raiseAlert() {
+        FileWriter write = new FileWriter("C:\\Users\\seanv\\Documents\\Sparrow\\diagnostics\\" + date + "+" + timereplaced + ".txt");
+        write.write("Sparrow Diagnostics File\nDATE: " + date + "\nTIME: " + time + "\n\n====================\n\nAdvanced status code recognition coming in future version. Your error status code was: " + i);
+        write.flush();
+        write.close();
+    }
+    public boolean raiseAlert() throws IOException {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(endpoint))
                 .header("Content-Type", "application/json")
@@ -31,10 +46,15 @@ public class SparrowClient {
                 .build();
         try {
             HttpResponse<Void> response = client.send(request, BodyHandlers.discarding());
-            return response.statusCode() == 201;
-        } catch(IOException | InterruptedException e) {
+            if (response.statusCode() != 201) {
+                createFile(response.statusCode());
+                return false;
+            }else{
+                return true;
+            }
+        } catch (IOException | InterruptedException e) {
+            createFile(7);
             return false;
         }
     }
-
 }
